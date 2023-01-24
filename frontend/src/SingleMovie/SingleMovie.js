af24e08a959cde050bad8e954ea6d7fc34461646
Riley Shipman <108430567+Cal-V@users.noreply.github.com>
2023-01-23 18:19:38 -0700

    extra styling

--- frontend/src/SingleMovie/SingleMovie.js
+++ frontend/src/SingleMovie/SingleMovie.js
@@ -4,6 +4,7 @@ function SingleMovie({movie}) {
 
     if (movie?.id) {
         return (
+<<<<<<< Updated upstream
             <div style={{textAlign: 'center'}}>
                 <div>
                     <img src={process.env.PUBLIC_URL + '/Images/pussinboots.jpg'}/>
@@ -19,6 +20,27 @@ function SingleMovie({movie}) {
                     <p>{movie.starring}</p>
                     <p>{movie.description}</p>
                     <p>{movie.IMDB}</p>
+=======
+            <div className='details-container'>
+                <div id="single-movie-holder">
+                    <div className='movie-img-box'><img id='single-movie-img' src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} /></div>
+                    <div id="movie-details">
+                        <div className='detail-block'><h2>{movie.title}</h2></div>
+                        <div className='top-border detail-block'><p>{movie.overview.replaceAll("--","—")}</p></div>
+                        <div className='top-border detail-block'><h3>Genres:</h3><ul>
+                            {movie.genres.map(genre => (
+                                <li>{genre.name}</li>
+                            ))}
+                        </ul></div>
+                        <div className='top-border detail-block'><p>Released: {date.toDateString()}</p></div>
+                        <div className='top-border detail-block'>
+                            <h3>Production Companies:</h3>
+                            <div id="company-logo-holder">{movie.production_companies.map(company => (
+                                <ProductionCompany company={company} />
+                            ))}</div>
+                        </div>
+                    </div>
+>>>>>>> Stashed changes
                 </div>
             </div>
         )
