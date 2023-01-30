fd0a05cdefaa95019bc8572b8845d5b1563f7146
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2023-01-29 20:15:57 -0700

    Finished styling for navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import axios from 'axios'
 import { useState } from 'react';
+import { Search } from "@material-ui/icons"
 import {Link} from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
 import './navbar.css'
 
 function Navbar({getRandomMovie}) {
+    const [user, setUser] = useState(true)
     const [movie,setMove] = useState({})
+
+    const handleClick = () => {
+        setUser(!user)
+    }
 
     getRandomMovie = () => {
         let movieID = Math.ceil(Math.random()*999999);
@@ -22,9 +28,12 @@ function Navbar({getRandomMovie}) {
                 <Link to="/">
                         <h1>Catgirl Film Reviews</h1>
                 </Link>
-                <div id='searchBar'>
-                    <input></input>
-                    <SearchIcon fontSize="large" className=''/>
+                <div className='topbar-center'>
+                    <div className='search-bar'>
+                        <SearchIcon className='search-icon'/>
+                        <input placeholder="Search..." type={Text} className="search-input"/>
+                        {/* SEARCH BAR IS NOT HOOKED UP TO FUNCTIONS, AND DOES NOTHING AT THE MOMENT */}
+                    </div>
                 </div>
                 <button onClick={getRandomMovie}>Random</button>
             
@@ -33,11 +42,28 @@ function Navbar({getRandomMovie}) {
                     Movies
                 </button>
             </Link>
-            <Link to="/user">
-                <button>
-                    User
-                </button>
-            </Link>
+            {user &&(
+                <div>
+                <span>
+                    <a href='/user' className='link'>                
+                        <button>
+                            Account
+                        </button>
+                    </a>
+                </span>
+                <button onClick={handleClick} className='logout'>Log out</button>
+              </div>
+              )}
+              {!user && (
+                <div>
+                  <Link to='' className='link'>
+                    <button onClick={handleClick} className='logout'>Log In</button>
+                  </Link>
+                  <Link to='' className='link'>
+                    <button onClick={handleClick} className='logout'>Sign Up</button>
+                  </Link>
+                </div>
+              )}
             </div>
         </nav>
     )
