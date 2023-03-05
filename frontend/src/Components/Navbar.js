911c46aed3a3752c090e1b707126660387ded162
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2023-03-05 15:49:17 -0700

    Added basic searching functionality

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {Link} from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
 import './navbar.css'
@@ -10,10 +10,27 @@ function Navbar({getRandomMovie}) {
     const [movieData,setMovieData] = useState([])
     const [similarMovieData,setSimilarMovieData] = useState([])
     const [movieID, setMovieID] = useState(0)
+    const [searchTerm, setSearchTerm] = useState('')
 
     const handleClick = () => {
         setUser(!user)
     }
+
+
+    const search = (e) => {
+        e.preventDefault()
+        alert(searchTerm)
+        //Search Database and return data for whatever was searched here
+    }
+
+    const changeSearch = event => {
+        event.preventDefault()
+        setSearchTerm(event.target.value)
+    }
+
+    useEffect(() => {
+      console.log(searchTerm)
+    }, [searchTerm])
 
     getRandomMovie = async () => {
         let movieID = Math.ceil(Math.random()*999999);
@@ -34,12 +51,12 @@ function Navbar({getRandomMovie}) {
                         <h1>Catgirl Film Reviews</h1>
                 </Link>
                 <div className='topbar-center'>
-                    <div className='search-bar'>
-                        <SearchIcon className='search-icon'/>
-                        <input placeholder="Search..." type='text' className="search-input"/>
+                    <form className='search-bar' onSubmit={search}>
+                        <SearchIcon className='search-icon' onClick={search}/>
+                        <input placeholder="Search..." type='text' className="search-input" value={searchTerm} onChange={changeSearch}/>
+                        
                         {/* SEARCH BAR IS NOT HOOKED UP TO FUNCTIONS, AND DOES NOTHING AT THE MOMENT */}
-                    </div>
-                    
+                    </form>
                 </div>
                 {user &&(
                 <div>
@@ -60,11 +77,6 @@ function Navbar({getRandomMovie}) {
                 <div>
                     <Link to="/movie" className=''>
                         <button onClick={getRandomMovie}>Random</button>
-                    </Link>
-                    <Link to="/movie" className=''>
-                        <button>
-                            Movies
-                        </button>
                     </Link>
                     <Link to='/login' className='link'>
                         <button onClick={handleClick} className='logout'>Log In</button>
--- frontend/src/Landing/Landing.js
+++ frontend/src/Landing/Landing.js
@@ -2,9 +2,27 @@ import React from 'react'
 import MoviesList from '../Components/MoviesList/MoviesList';
 import moviedata from '../moviedummydata.json'
 import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect, useRef } from 'react';
 import "./landing.css"
 
 const Landing = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const search = (e) => {
+      e.preventDefault()
+      alert(searchTerm)
+      //Search Database and return data for whatever was searched here
+  }
+
+  const changeSearch = event => {
+      event.preventDefault()
+      setSearchTerm(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log(searchTerm)
+  }, [searchTerm])
+
   return (
     <div className='bg'>
       
@@ -12,11 +30,12 @@ const Landing = () => {
           <h1 style={{marginTop: '50px', marginLeft: '70px'}} className="landinglogo"><img src={process.env.PUBLIC_URL + '/Images/logopink.png'} className="landinglogoimg"/>
             <span className='logotext' style={{fontSize: '100px'}}>Catgirl Film Reviews</span>
             <div className='topbar-center' style={{margin:'auto',fontSize: '20px'}}>
-              <div className='search-bar'>
-                  <SearchIcon className='search-icon'/>
-                  <input placeholder="Search..." type='text' className="search-input"/>
-                  {/* SEARCH BAR IS NOT HOOKED UP TO FUNCTIONS, AND DOES NOTHING AT THE MOMENT */}
-              </div>
+              <form className='search-bar' onSubmit={search}>
+                <SearchIcon className='search-icon' onClick={search}/>
+                <input placeholder="Search..." type='text' className="search-input" value={searchTerm} onChange={changeSearch}/>
+                
+                {/* SEARCH BAR IS NOT HOOKED UP TO FUNCTIONS, AND DOES NOTHING AT THE MOMENT */}
+              </form>
             </div>
           </h1>
         </div>
